01123dda801ceebb34763aa20a8883a336e7c3bd
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2026-05-03 18:14:47 -0400

    java

---
+++ 001. Lambdas/007. Crear un Predicate/Main.java
@@ -0,0 +1,17 @@
+package Modern_java_backend_roadmap.Lambda.Ejercicio_7_Crear_Predicate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+        Predicate<String> esLargo = s -> s.length() > 4;
+
+        List<String> names = new ArrayList<>(List.of("Ana","Luis","Pedro", "Alberto"));
+
+        names.removeIf(esLargo.negate());
+
+        names.forEach(System.out::println);
+    }
+}
